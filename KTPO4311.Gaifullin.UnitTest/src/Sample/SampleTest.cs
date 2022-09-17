b18f251eabfa5efcd0e520e03b8a0b283ca6af2a
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-09-17 12:07:09 +0300

    Добавьте файлы проекта.

---
+++ KTPO4311.Gaifullin.UnitTest/src/Sample/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+namespace KTPO4311.Gaifullin.UnitTest.src.Sample
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void Demo()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
