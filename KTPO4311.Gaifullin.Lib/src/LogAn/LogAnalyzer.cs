1716f810b1b5d32ce1b3c7ec854a1f7709b58240
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-09-17 12:25:59 +0300

    Добавлен новый проект в решение.

---
+++ KTPO4311.Gaifullin.Lib/src/LogAn/LogAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTPO4311.Gaifullin.Lib.src.LogAn
+{
+    internal class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if(fileName.EndsWith(".GDR"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
