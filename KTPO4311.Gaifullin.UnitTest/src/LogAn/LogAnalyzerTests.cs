2e333938334be78ecadc20271b62f7a3b6933a57
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-10-01 13:37:36 +0300

    5. FakeExtension

--- KTPO4311.Gaifullin.UnitTest/src/LogAn/LogAnalyzerTests.cs
+++ KTPO4311.Gaifullin.UnitTest/src/LogAn/LogAnalyzerTests.cs
@@ -5,6 +5,76 @@ namespace KTPO4311.Gaifullin.UnitTest.src.LogAn
     [TestFixture]
     public class LogAnalyzerTests
     {
-       
+        [Test]
+        public void IsValidFileName_NameSupportedExtension_ReturnsTrue()
+        {
+            FakeExtensionManager fakeManager = new FakeExtensionManager();
+            fakeManager.WillBeValid = true;
+
+            LogAnalyzer log = new LogAnalyzer(fakeManager);
+
+            //Воздействие на тестируемый объект
+            bool result = log.IsValidLogFileName("short.gdr");
+
+            //Проверка ожидаемого результата
+            Assert.True(result);
+        }
+
+        [Test]
+        public void IsValidFileName_NameNotSupportedExtension_ReturnsFalse()
+        {
+            FakeExtensionManager fakeManager = new FakeExtensionManager();
+            fakeManager.WillBeValid = false;
+
+            LogAnalyzer log = new LogAnalyzer(fakeManager);
+
+            //Воздействие на тестируемый объект
+            bool result = log.IsValidLogFileName("long.gdr");
+
+            //Проверка ожидаемого результата
+            Assert.False(result);
+        }
+
+        [Test]
+        public void IsValidFileName_ExtManagerThrowsException_ReturnFalse()
+        {
+            //Подготовка теста
+            FakeExtensionManager fakeManager = new FakeExtensionManager();
+            fakeManager.WillBeValid = false;
+            fakeManager.WillThrow = new Exception();
+
+            LogAnalyzer log = new LogAnalyzer(fakeManager);
+
+            //Воздействие на тестируемый объект
+            bool result = log.IsValidLogFileName("short.gdr");
+
+            //Проверка ожидаемого результата
+            Assert.False(result);
+        }
+    }
+
+    /// <summary>
+    /// Поддельный менеджер расширений
+    /// </summary>
+    internal class FakeExtensionManager : IExtensionManager
+    {
+        ///<summary>Это поле позволяет настроить
+        ///поддельный результат для метода IsValid</summary>
+        public bool WillBeValid = false;
+
+        /// <summary>
+        /// Это поле позволяет настроить поддельное 
+        /// исключение вызываемое в методе IsValid
+        /// </summary>
+        public Exception WillThrow = null;
+
+        public bool IsValid(string fileName)
+        {
+            if(WillThrow != null)
+            {
+                return false;
+            }
+            return WillBeValid;
+        }
     }
 }
