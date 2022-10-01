f7508fc230e484af53d722aa64bddb9d735a681a
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-10-01 11:51:24 +0300

    1. Подготовка проекта

--- KTPO4311.Gaifullin.Lib/src/LogAn/LogAnalyzer.cs
+++ KTPO4311.Gaifullin.Lib/src/LogAn/LogAnalyzer.cs
@@ -1,21 +1,18 @@
 ﻿namespace KTPO4311.Gaifullin.Lib.src.LogAn
 {
+    /// <summary>
+    /// Анализатор лог. файлов
+    /// </summary>
     public class LogAnalyzer
     {
-
-        public bool WasLastFileNameIsValid { get; set; }
+        /// <summary>
+        /// Проверка правильности имени файла
+        /// </summary>
         public bool IsValidLogFileName(string fileName)
         {
-            WasLastFileNameIsValid = false;
-            if (string.IsNullOrEmpty(fileName))
-            {
-                throw new ArgumentException("Имя файла должно быть задано.");
-            }
-            if (!fileName.EndsWith(".GDR", StringComparison.CurrentCultureIgnoreCase))
-            {
-                return false;
-            }
-            WasLastFileNameIsValid = true;
+            //читать конфигурационный файл
+            //вернуть true
+            //если конфигурация поддерживается
             return true;
         }
     }
--- KTPO4311.Gaifullin.UnitTest/src/LogAn/LogAnalyzerTests.cs
+++ KTPO4311.Gaifullin.UnitTest/src/LogAn/LogAnalyzerTests.cs
@@ -6,52 +6,16 @@ namespace KTPO4311.Gaifullin.UnitTest.src.LogAn
     public class LogAnalyzerTests
     {
         [Test]
-        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        public void IsValidFileName_ReturnsTrue()
         {
+            //Подготовка теста
             LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
-            Assert.False(result);
-        }
-        [Test]
-        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("filewithgoodextension.GDR");
+
+            //Воздействие на тестируемый объект
+            bool result = analyzer.IsValidLogFileName("f.GDR");
+
+            //Проверка ожидаемого результата
             Assert.True(result);
         }
-        [Test]
-        public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName("filewithgoodextension.gdr");
-            Assert.True(result);
-        }
-        [TestCase("filewithgoodextension.GDR")]
-        [TestCase("filewithgoodextension.gdr")]
-        public void IsValidLogFileName_ValidExtension_ReturnsTrue(string fileName)
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = analyzer.IsValidLogFileName(fileName);
-            Assert.True(result);
-        }
-        [Test]
-        public void IsValidFileName_EmptyFileName_Throws()
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-
-            var ex = Assert.Catch<Exception>(() => analyzer.IsValidLogFileName(""));
-
-            StringAssert.Contains("Имя файла должно быть задано", ex.Message);
-        }
-        [TestCase("badfile.foo", false)]
-        [TestCase("goodfile.gdr", true)]
-        public void IsValidFileName_WhenCalled_ChangesWasLastFilenameValid(string file, bool expected)
-        {
-            LogAnalyzer analyzer = new LogAnalyzer();
-            analyzer.IsValidLogFileName(file);
-            Assert.AreEqual(expected, analyzer.WasLastFileNameIsValid);
-        }
-
-
     }
 }
