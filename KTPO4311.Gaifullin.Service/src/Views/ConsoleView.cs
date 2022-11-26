eac53c01d8c108dca1cfc6aff3e3cd59dbb8ab8f
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-11-26 11:31:16 +0300

    1

---
+++ KTPO4311.Gaifullin.Service/src/Views/ConsoleView.cs
@@ -0,0 +1,13 @@
+﻿using KTPO4311.Gaifullin.Lib.src.LogAn;
+
+namespace KTPO4311.Gaifullin.Service.src.Views
+{
+    public class ConsoleView : IView
+    {
+        public void Render(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
+
